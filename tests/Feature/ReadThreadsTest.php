0f5cde94ab425bf095bc77aed9f37591d2eda42d
Ian Travers <mr.itpe@gmail.com>
2020-02-14 15:37:29 +0300

    Add replies to thread show page

---
+++ tests/Feature/ReadThreadsTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Reply;
+use App\Thread;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class ReadThreadsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * @var Thread
+     */
+    protected $thread;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->thread = factory(Thread::class)->create();
+    }
+
+    /** @test */
+    public function user_can_view_all_threads()
+    {
+        $this->get('/threads')
+            ->assertSee($this->thread->title);
+    }
+
+    /** @test */
+    function user_can_rad_single_thread()
+    {
+        $this->get('/threads/' . $this->thread->id)
+            ->assertSee($this->thread->title);
+    }
+
+    /** @test */
+    function user_can_read_replies_that_associated_this_a_thread()
+    {
+        $reply = factory(Reply::class)->create(['thread_id' => $this->thread->id]);
+
+        $this->get('/threads/' . $this->thread->id)
+            ->assertSee($reply->owner->name)
+            ->assertSee($reply->body);
+    }
+}
