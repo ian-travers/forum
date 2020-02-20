b41e6291a4aced06f3e4322f90a0ffad5f12cd47
Ian Travers <mr.itpe@gmail.com>
2020-02-20 14:22:09 +0300

    Refactor thread create page (use channel and old values)

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -13,14 +13,38 @@
 
                             @csrf
                             <div class="form-group">
+                                <label for="channel_id">Choose a Channel</label>
+                                <select name="channel_id" id="channel_id" class="form-control" required>
+                                    <option value="">Choose One ...</option>
+                                    @foreach(\App\Channel::all() as $channel)
+                                        @php /** @var \App\Channel $channel */ @endphp
+                                        <option
+                                            value="{{ $channel->id }}" {{ old('channel_id') == $channel->id ? 'selected' : '' }}>{{ $channel->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="form-group">
                                 <label for="title">Title</label>
-                                <input name="title" type="text" class="form-control" id="title">
+                                <input name="title" type="text" class="form-control" id="title"
+                                       value="{{ old('title') }}" required>
                             </div>
                             <div class="form-group">
                                 <label for="body">Body</label>
-                                <textarea name="body" id="body" rows="7" class="form-control"></textarea>
+                                <textarea name="body" id="body" rows="7"
+                                          class="form-control" required>{{ old('body') }}</textarea>
                             </div>
                             <button type="submit" class="btn btn-primary">Publish</button>
+
+                            @if(count($errors))
+                                <ul class="alert alert-danger mt-3">
+
+                                    @foreach($errors->all() as $error)
+                                        <li class="ml-2">{{ $error }}</li>
+
+                                    @endforeach
+                                </ul>
+
+                            @endif
                         </form>
                     </div>
                 </div>
