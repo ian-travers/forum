c2b71294fa948eaaf4d3cc43c9617c07966a2965
Ian Travers <mr.itpe@gmail.com>
2020-02-25 13:15:43 +0300

    Delete related replies (option #2)

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -68,7 +68,6 @@ class ThreadsController extends Controller
 
     public function destroy($channel, Thread $thread)
     {
-        $thread->replies()->delete();
         $thread->delete();
 
         return response([], Response::HTTP_NO_CONTENT);
--- app/Thread.php
+++ app/Thread.php
@@ -46,6 +46,10 @@ class Thread extends Model
         static::addGlobalScope('repliesCount', function (Builder $builder) {
             $builder->withCount('replies');
         });
+
+        static::deleting(function (Thread $thread){
+            $thread->replies()->delete();
+        });
     }
 
     public function path(): string
