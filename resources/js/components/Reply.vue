b3e13f8e3fc9b40e1625478adc549ec176dce421
Ian Travers <mr.itpe@gmail.com>
2020-02-29 09:44:40 +0300

    Make thread-view vue component && use collection of replies

--- resources/js/components/Reply.vue
+++ resources/js/components/Reply.vue
@@ -1,21 +1,61 @@
+<template>
+    <div :id="'reply-' + id" class="card mt-3">
+        <div class="card-header">
+            <div class="d-flex justify-content-between">
+                <div>
+                    <a :href="'/profile/' + data.owner.name" v-text="data.owner.name"></a>
+                    said {{ data.created_at }}...
+                </div>
+                <div>
+
+<!--                    @if(auth()->check())-->
+<!--                    <favorite :reply="{{ $reply }}"></favorite>-->
+
+<!--                    @endif-->
+                </div>
+            </div>
+
+        </div>
+        <div class="card-body">
+            <div v-if="editing">
+                <div class="form-group">
+                    <textarea class="form-control" v-model="body" name=""></textarea>
+                </div>
+                <button class="btn btn-sm btn-primary" @click="update">Update</button>
+                <button class="btn btn-sm btn-secondary" @click="editing = false">Cancel</button>
+            </div>
+            <div v-else v-text="body"></div>
+        </div>
+
+<!--        @can('update', $reply)-->
+        <div class="card-footer">
+            <button class="btn btn-sm btn-primary" @click="editing = true">Edit</button>
+            <button class="btn btn-sm btn-danger" @click="destroy">Delete</button>
+        </div>
+
+<!--        @endcan-->
+    </div>
+</template>
+
 <script>
     import Favorite from "./Favorite";
 
     export default {
-        props: ['attributes'],
+        props: ['data'],
 
         components: {Favorite},
 
         data() {
             return {
                 editing: false,
-                body: this.attributes.body
+                id: this.data.id,
+                body: this.data.body
             };
         },
 
         methods: {
             update() {
-                axios.patch('/replies/' + this.attributes.id, {
+                axios.patch('/replies/' + this.data.id, {
                     body: this.body
                 });
 
@@ -25,7 +65,7 @@
             },
 
             destroy() {
-                axios.delete('/replies/' + this.attributes.id);
+                axios.delete('/replies/' + this.data.id);
 
                 $(this.$el).fadeOut(600, () => {
                     flash('Your reply has been deleted.');
--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -3,73 +3,78 @@
 @php /* @var App\Thread $thread */ @endphp
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-9">
-                <div class="card">
-                    <div class="card-header">
-                        <div class="d-flex align-items-baseline justify-content-between">
-                            <div>
-                                <a href="{{ route('profile', $thread->creator) }}"
-                                   class="">{{ $thread->creator->name }} </a> posted:
-                                {{ $thread->title }}
+    <thread-view inline-template>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-9">
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="d-flex align-items-baseline justify-content-between">
+                                <div>
+                                    <a href="{{ route('profile', $thread->creator) }}"
+                                       class="">{{ $thread->creator->name }} </a> posted:
+                                    {{ $thread->title }}
+                                </div>
+
+                                @can('update', $thread)
+                                    <form action="{{ $thread->path() }}" method="post">
+
+                                        @csrf
+                                        @method('delete')
+                                        <button type="submit" class="btn btn-link">Delete Thread</button>
+                                    </form>
+
+                                @endcan
                             </div>
 
-                            @can('update', $thread)
-                                <form action="{{ $thread->path() }}" method="post">
-
-                                    @csrf
-                                    @method('delete')
-                                    <button type="submit" class="btn btn-link">Delete Thread</button>
-                                </form>
-
-                            @endcan
                         </div>
-
+                        <div class="card-body">
+                            {{ $thread->body }}
+                        </div>
                     </div>
-                    <div class="card-body">
-                        {{ $thread->body }}
-                    </div>
-                </div>
 
-                @foreach($replies as $reply)
-                    @include('threads.reply')
-                @endforeach
+                    <replies :data="{{ $thread->replies }}"></replies>
 
-                <div class="mt-3">
-                    {{ $replies->links() }}
-                </div>
+                    {{--                @foreach($replies as $reply)--}}
+                    {{--                    @include('threads.reply')--}}
+                    {{--                @endforeach--}}
+                    {{----}}
+                    {{--                <div class="mt-3">--}}
+                    {{--                    {{ $replies->links() }}--}}
+                    {{--                </div>--}}
 
 
-                @if(auth()->check())
-                    <form action="{{ $thread->path() . '/replies' }}" method="post">
+                    @if(auth()->check())
+                        <form action="{{ $thread->path() . '/replies' }}" method="post">
 
-                        @csrf
-                        <div class="form-group mt-3">
+                            @csrf
+                            <div class="form-group mt-3">
                             <textarea name="body" id="body" class="form-control" rows="5"
                                       placeholder="Have something to say?"></textarea>
-                        </div>
-                        <button type="submit" class="btn btn-primary">Post</button>
-                    </form>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Post</button>
+                        </form>
 
-                @else
-                    <p class="text-center mt-3">Please <a href="{{ route('login') }}">sign in</a> to participate to this
-                        discussion.</p>
-                @endif
-            </div>
-            <div class="col-md-3">
-                <div class="card">
-                    <div class="card-body">
-                        <p>
-                            This thread was published {{ $thread->created_at->diffForHumans() }} by
-                            <a href="{{ route('profile', $thread->creator) }}">{{ $thread->creator->name }}</a>, and
-                            currently
-                            has {{ $thread->replies_count }} {{ \Illuminate\Support\Str::plural('comment', $thread->replies_count) }}
-                            .
-                        </p>
+                    @else
+                        <p class="text-center mt-3">Please <a href="{{ route('login') }}">sign in</a> to participate to this
+                            discussion.</p>
+                    @endif
+                </div>
+                <div class="col-md-3">
+                    <div class="card">
+                        <div class="card-body">
+                            <p>
+                                This thread was published {{ $thread->created_at->diffForHumans() }} by
+                                <a href="{{ route('profile', $thread->creator) }}">{{ $thread->creator->name }}</a>, and
+                                currently
+                                has {{ $thread->replies_count }} {{ \Illuminate\Support\Str::plural('comment', $thread->replies_count) }}
+                                .
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    </thread-view>
+
 @endsection
