5eb7d2ce19a99365d593639d507e6b82ad1dc07c
Ian Travers <mr.itpe@gmail.com>
2020-03-02 14:04:03 +0300

    Check auth for new reply

--- resources/js/components/NewReply.vue
+++ resources/js/components/NewReply.vue
@@ -1,18 +1,15 @@
 <template>
     <div>
-        <!--        @if(auth()->check())-->
-        <!--        <form action="{{ $thread->path() . '/replies' }}" method="post">-->
-        <div class="form-group mt-3">
+        <div v-if="signedIn">
+            <div class="form-group mt-3">
             <textarea name="body" id="body" class="form-control" rows="5"
                       placeholder="Have something to say?" v-model="body" required></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary" @click="addReply">Post</button>
         </div>
-        <button type="submit" class="btn btn-primary" @click="addReply">Post</button>
-        <!--        </form>-->
 
-        <!--        @else-->
-        <!--        <p class="text-center mt-3">Please <a href="{{ route('login') }}">sign in</a> to participate to this-->
-        <!--            discussion.</p>-->
-        <!--        @endif-->
+        <p class="text-center mt-3" v-else>Please <a href="/login">sign in</a> to participate to this
+            discussion.</p>
     </div>
 </template>
 
@@ -22,6 +19,12 @@
             return {
                 body: '',
                 endpoint: '/threads/et/53/replies'
+            }
+        },
+
+        computed: {
+            signedIn() {
+                return window.App.signedIn;
             }
         },
 
