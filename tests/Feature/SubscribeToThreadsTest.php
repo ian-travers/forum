0ecad6914e62db23c1391cebb84a8632714eb775
Ian Travers <mr.itpe@gmail.com>
2020-03-05 11:28:35 +0300

    Prepare notifications for subscribed users

---
+++ tests/Feature/SubscribeToThreadsTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Thread;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class SubscribeToThreadsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    function user_can_subscribe_to_threads()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->signIn();
+
+        /** @var Thread $thread */
+        $thread = create(Thread::class);
+
+        $this->post($thread->path() . '/subscriptions');
+
+        $this->assertCount(1, $thread->subscriptions);
+
+        $thread->addReply([
+            'user_id' => auth()->id(),
+            'body' => 'Some reply here',
+        ]);
+
+
+    }
+}
