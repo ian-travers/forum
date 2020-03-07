35ae7ce3b4b1e5998398d74944ef2bbd5644300d
Ian Travers <mr.itpe@gmail.com>
2020-03-07 13:40:25 +0300

    Mark a notification as read (upgrade UserNotifications.vue)

--- resources/js/components/UserNotifications.vue
+++ resources/js/components/UserNotifications.vue
@@ -5,10 +5,13 @@
         </a>
 
         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-            <div v-for="notification in notifications" class="dropdown-item notification-item">
+            <div v-for="(notification, index) in notifications" class="dropdown-item notification-item">
                 <strong v-text="notification.data.author"></strong>
                 <span v-text="notification.data.action"></span><br>
-                <a :href="notification.data.link" v-text="notification.data.thread"></a><br>
+                <a :href="notification.data.link"
+                    v-text="notification.data.thread"
+                    @click="markAsRead(notification, index)"
+                ></a><br>
                 <span v-text="notification.data.at" class="float-right small"></span><br>
             </div>
         </div>
@@ -19,13 +22,20 @@
     export default {
         data() {
             return {
-                notifications: false
+                notifications: []
             }
         },
 
         created() {
             axios.get('/profiles/' + window.App.user.name + '/notifications')
                 .then(response => this.notifications = response.data)
+        },
+
+        methods: {
+            markAsRead(notification, index) {
+                axios.delete('/profiles/' + window.App.user.name + '/notifications/' + notification.id);
+                this.notifications.splice(index, 1);
+            }
         }
     }
 </script>
