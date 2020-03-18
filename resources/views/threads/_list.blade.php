c11d7df569bbc2a3149de610fa1cba00c133bb37
Ian Travers <mr.itpe@gmail.com>
2020-03-18 10:38:38 +0300

    Extract threads view && use named profile href

---
+++ resources/views/threads/_list.blade.php
@@ -0,0 +1,38 @@
+@php /* @var App\Thread $thread */ @endphp
+@forelse($threads as $thread)
+    <div class="card mb-3">
+        <div class="card-header">
+            <div class="d-flex justify-content-between">
+                <h4>
+                    <a href="{{ $thread->path() }}">
+
+                        @if(auth()->check() && $thread->hasUpdatesFor(auth()->user()))
+                            <strong>{{ $thread->title }}</strong>
+
+                        @else
+                            {{ $thread->title }}
+
+                        @endif
+                    </a>
+                </h4>
+                <a href="{{ $thread->path() }}">
+                    {{ $thread->replies_count }}
+                    {{ \Illuminate\Support\Str::plural('comment', $thread->replies_count) }}
+                </a>
+            </div>
+            <div>
+                <small class="fas fa-user"></small>
+                <a href="{{ route('profile', $thread->creator) }}"> {{ $thread->creator->name }}</a>
+                <small class="fas fa-calendar-day"></small>
+                <span> {{ $thread->created_at->format('H:i l F d, Y.') }}</span>
+            </div>
+        </div>
+        <div class="card-body">
+            <div>{{ $thread->body }}</div>
+        </div>
+    </div>
+
+@empty
+    <p>There is not relevant results at this time</p>
+
+@endforelse
