db806d0c9262ab556b1177b2e771de8cb65c1f6e
Ian Travers <mr.itpe@gmail.com>
2020-03-21 16:25:45 +0300

    Upgrade Trending class

--- app/Trending.php
+++ app/Trending.php
@@ -6,16 +6,26 @@ use Illuminate\Support\Facades\Redis;
 
 class Trending
 {
+    public function cacheKey()
+    {
+        return app()->environment('testing') ? 'testing_trending_threads' : 'trending_threads';
+    }
+
     public function get()
     {
-        return array_map('json_decode', Redis::zrevrange('trending_threads', 0, 4));
+        return array_map('json_decode', Redis::zrevrange($this->cacheKey(), 0, 4));
     }
 
     public function push(Thread $thread)
     {
-        Redis::zincrby('trending_threads', 1, json_encode([
+        Redis::zincrby($this->cacheKey(), 1, json_encode([
             'title' => $thread->title,
             'path' => $thread->path(),
         ]));
+    }
+
+    public function reset()
+    {
+        Redis::del($this->cacheKey());
     }
 }
--- tests/Feature/TrendingThreadsTest.php
+++ tests/Feature/TrendingThreadsTest.php
@@ -3,39 +3,43 @@
 namespace Tests\Feature;
 
 use App\Thread;
+use App\Trending;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Support\Facades\Redis;
 use Tests\TestCase;
 
 class TrendingThreadsTest extends TestCase
 {
     use DatabaseMigrations;
 
+    /**
+     * @var Trending
+     */
+    private $trending;
+
     protected function setUp(): void
     {
         parent::setUp();
 
-        Redis::del('trending_threads');
+        $this->trending = new Trending();
+
+        $this->trending->reset();
     }
 
     /** @test */
     function it_increments_a_thread_score_each_time_it_is_read()
     {
-        $this->assertCount(0, Redis::zrevrange('trending_threads', 0, -1));
+        $this->assertCount(0, $this->trending->get());
 
         /** @var Thread $thread */
         $thread = create(Thread::class);
 
         $this->call('get', $thread->path());
 
-
-        $trending = Redis::zrevrange('trending_threads', 0, -1);
+        $trending = $this->trending->get();
 
         $this->assertCount(1, $trending);
-        $this->assertEquals($thread->title, json_decode($trending[0])->title);
+        $this->assertEquals($thread->title, $trending[0]->title);
 
-        Redis::del('trending_threads');
+        $this->trending->reset();
     }
-
-
 }
