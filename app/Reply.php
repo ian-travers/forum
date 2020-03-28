9a034d876e408bc543860973a7bd7c5337755d25
Ian Travers <mr.itpe@gmail.com>
2020-03-28 10:53:37 +0300

    Remember the best reply (wip)

--- app/Reply.php
+++ app/Reply.php
@@ -40,7 +40,7 @@ class Reply extends Model
 
     protected $with = ['owner', 'favorites', 'thread'];
 
-    protected $appends = ['favoritesCount', 'isFavorited'];
+    protected $appends = ['favoritesCount', 'isFavorited', 'isBest'];
 
     protected static function boot()
     {
@@ -91,5 +91,10 @@ class Reply extends Model
     public function isBest()
     {
         return $this->thread->best_reply_id == $this->id;
+    }
+
+    public function getIsBestAttribute()
+    {
+        return $this->isBest();
     }
 }
--- resources/js/components/Reply.vue
+++ resources/js/components/Reply.vue
@@ -61,7 +61,7 @@
                 editing: false,
                 id: this.data.id,
                 body: this.data.body,
-                isBest: false,
+                isBest: this.data.isBest,
                 reply: this.data,
             };
         },
@@ -70,6 +70,12 @@
             ago() {
                 return moment(this.data.created_at).fromNow();
             }
+        },
+
+        created() {
+            window.events.$on('best-reply-selected', id => {
+                this.isBest = (id === this.id)
+            });
         },
 
         methods: {
@@ -98,7 +104,9 @@
             },
 
             markBestReply() {
-                this.isBest = true;
+                axios.post('/replies/' + this.reply.id + '/best');
+
+                window.events.$emit('best-reply-selected', this.reply.id);
             }
         }
     }
