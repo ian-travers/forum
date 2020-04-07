041aa1c7c8a19da1dbbd25753bc6a2a05576b2ab
Ian Travers <mr.itpe@gmail.com>
2020-04-07 16:21:07 +0300

    Network latency live hack for testing algolia service

--- tests/Feature/SearchTest.php
+++ tests/Feature/SearchTest.php
@@ -20,9 +20,14 @@ class SearchTest extends TestCase
         create(Thread::class, [], 2);
         create(Thread::class, ['body' => "Thread with the {$search} term"], 2);
 
-        $results = $this->getJson("/threads/search?q={$search}")->json();
+        do {
+            sleep(.5);
 
-        $this->assertCount(2, $results['data']);
+            $results = $this->getJson("/threads/search?q={$search}")->json()['data'];
+        } while(empty($results));
+
+
+        $this->assertCount(2, $results);
 
         Thread::latest()->take(4)->unsearchable();
     }
