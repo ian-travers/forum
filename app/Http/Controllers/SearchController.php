e457c62505e90b6d0845fb3552f591813db933bd
Ian Travers <mr.itpe@gmail.com>
2020-04-07 15:57:19 +0300

    Separate search results to view and json

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -3,13 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\Thread;
+use App\Trending;
 
 class SearchController extends Controller
 {
-    public function show()
+    public function show(Trending $trending)
     {
         $search = request('q');
 
-        return Thread::search($search)->paginate(20);
+        $threads = Thread::search($search)->paginate(20);
+
+        if (request()->expectsJson()) {
+            return $threads;
+        }
+
+        return view('threads.index', [
+            'threads' => $threads,
+            'trending' => $trending->get()
+        ]);
     }
 }
