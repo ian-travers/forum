4ba2609b7f30b6eacf802fefeac338b83eda5458
Ian Travers <mr.itpe@gmail.com>
2020-04-07 16:34:02 +0300

    Search in threads (frontend)

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -16,6 +16,24 @@
                 @endif
             </div>
             <div class="col-md-3">
+                <div class="card mb-3">
+                    <div class="card-header">
+                        Search
+                    </div>
+                    <div class="card-body">
+                        <form method="get" action="{{ route('threads.search') }}">
+                            <div class="input-group">
+                                <input type="text" class="form-control" value="{{ request('q') }}" name="q"
+                                       placeholder="Search for something..." autofocus>
+                                <div class="input-group-append">
+                                    <button class="btn btn-outline-secondary" type="submit">
+                                        <i class="fas fa-search"></i>
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
 
                 @if(count($trending))
                     <div class="card">
