70bc6254ef5a887f82e964f0f99173998ab58d6a
Ian Travers <mr.itpe@gmail.com>
2020-04-10 18:52:05 +0300

    Edit reply WYSIWYG

--- resources/js/components/Reply.vue
+++ resources/js/components/Reply.vue
@@ -16,7 +16,7 @@
             <div v-if="editing">
                 <form @submit="update">
                     <div class="form-group">
-                        <textarea class="form-control" v-model="body" required></textarea>
+                        <wysiwyg v-model="body"></wysiwyg>
                     </div>
                     <button class="btn btn-sm btn-primary">Update</button>
                     <button class="btn btn-sm btn-secondary" @click="cancel" type="button">Cancel</button>
